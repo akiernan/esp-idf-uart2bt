e4c424b63a1a47d825e9595f46a202e3ed38e1f9
Alex Kiernan <alex.kiernan@gmail.com>
2025-10-13 10:19:56 +0100

    Add conversion commands

--- nimble_ble/main/main.c
+++ nimble_ble/main/main.c
@@ -19,6 +19,10 @@
 #include "esp_event.h"
 #include "driver/uart.h"
 
+#include "argtable3/argtable3.h"
+
+#include "mesh/state_binding.h"
+
 #include "console_simple_init.h"
 #include "cmd_nvs.h"
 #include "cmd_system.h"
@@ -30,37 +34,68 @@ static const char *TAG = "MAIN";
 QueueHandle_t xQueueSpp;
 QueueHandle_t xQueueUart;
 
-#if 0
-static int do_hello_cmd(int argc, char **argv)
+static struct {
+	struct arg_int *actual;
+	struct arg_end *end;
+} bt_mesh_convert_lightness_actual_to_linear_args;
+
+static struct {
+	struct arg_int *linear;
+	struct arg_end *end;
+} bt_mesh_convert_lightness_linear_to_actual_args;
+
+static int do_bt_mesh_convert_lightness_actual_to_linear(int argc, char **argv)
 {
-	printf("Hello world!\n");
+	int nerrors = arg_parse(argc, argv, (void **)&bt_mesh_convert_lightness_actual_to_linear_args);
+
+	if (nerrors != 0) {
+		arg_print_errors(stderr, bt_mesh_convert_lightness_actual_to_linear_args.end, argv[0]);
+		return 0;
+	}
+
+	uint16_t lightness_actual = bt_mesh_convert_lightness_actual_to_linear_args.actual->ival[0];
+	printf("%d\n", bt_mesh_convert_lightness_actual_to_linear(lightness_actual));
 	return 0;
 }
 
-esp_err_t console_cmd_hello_register(void)
+static int do_bt_mesh_convert_lightness_linear_to_actual(int argc, char **argv)
 {
-	esp_err_t ret;
+	int nerrors = arg_parse(argc, argv, (void **)&bt_mesh_convert_lightness_linear_to_actual_args);
 
-	const esp_console_cmd_t hello_cmd = {
-		.command = "hello",
-		.help = "say hello",
-		.hint = NULL,
-		.func = do_hello_cmd,
-		.argtable = NULL
-	};
+	if (nerrors != 0) {
+		arg_print_errors(stderr, bt_mesh_convert_lightness_linear_to_actual_args.end, argv[0]);
+		return 0;
+	}
 
-	ret = esp_console_cmd_register(&hello_cmd);
-	if (ret)
-		ESP_LOGE(TAG, "Unable to register hello");
-
-	return ret;
+	uint16_t lightness_linear = bt_mesh_convert_lightness_linear_to_actual_args.linear->ival[0];
+	printf("%d\n", bt_mesh_convert_lightness_linear_to_actual(lightness_linear));
+	return 0;
 }
 
-static const console_cmd_plugin_desc_t __attribute__((section(".console_cmd_desc"), used)) PLUGIN = {
-    .name = "console_hello_cmd",
-    .plugin_regd_fn = &console_cmd_hello_register
-};
-#endif
+void register_commands(void)
+{
+	bt_mesh_convert_lightness_actual_to_linear_args.actual = arg_int1(NULL, NULL, "<actual>", "Light lightness actual");
+	bt_mesh_convert_lightness_actual_to_linear_args.end = arg_end(1);
+	const esp_console_cmd_t bt_mesh_convert_lightness_actual_to_linear_args_cmd = {
+		.command = "bt_mesh_convert_lightness_actual_to_linear",
+		.help = "Convert lightness actual to linear",
+		.hint = NULL,
+		.func = do_bt_mesh_convert_lightness_actual_to_linear,
+		.argtable = &bt_mesh_convert_lightness_actual_to_linear_args
+	};
+	ESP_ERROR_CHECK(esp_console_cmd_register(&bt_mesh_convert_lightness_actual_to_linear_args_cmd));
+
+	bt_mesh_convert_lightness_linear_to_actual_args.linear = arg_int1(NULL, NULL, "<linear>", "Light lightness linear");
+	bt_mesh_convert_lightness_linear_to_actual_args.end = arg_end(1);
+	const esp_console_cmd_t bt_mesh_convert_lightness_linear_to_actual_args_cmd = {
+		.command = "bt_mesh_convert_lightness_linear_to_actual",
+		.help = "Convert lightness linear to actual",
+		.hint = NULL,
+		.func = do_bt_mesh_convert_lightness_linear_to_actual,
+		.argtable = &bt_mesh_convert_lightness_linear_to_actual_args
+	};
+	ESP_ERROR_CHECK(esp_console_cmd_register(&bt_mesh_convert_lightness_linear_to_actual_args_cmd));
+}
 
 static void uart_init(void)
 {
@@ -80,20 +115,25 @@ static void uart_init(void)
 	// We won't use a buffer for sending data.
 	uart_driver_install(CONFIG_UART_NUM, UART_HW_FIFO_LEN(CONFIG_UART_NUM) * 2, 0, 0, NULL, 0);
 	uart_param_config(CONFIG_UART_NUM, &uart_config);
-	uart_set_pin(CONFIG_UART_NUM, CONFIG_UART_TX_GPIO, CONFIG_UART_RX_GPIO, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+	uart_set_pin(CONFIG_UART_NUM, CONFIG_UART_TX_GPIO, CONFIG_UART_RX_GPIO, UART_PIN_NO_CHANGE,
+		     UART_PIN_NO_CHANGE);
 }
 
-static void uart_tx_task(void* pvParameters)
+static void uart_tx_task(void *pvParameters)
 {
-	ESP_LOGI(pcTaskGetName(NULL), "Start using UART%d(GPIO%d)", CONFIG_UART_NUM, CONFIG_UART_TX_GPIO);
+	ESP_LOGI(pcTaskGetName(NULL), "Start using UART%d(GPIO%d)", CONFIG_UART_NUM,
+		 CONFIG_UART_TX_GPIO);
 	CMD_t cmdBuf;
-	while(1) {
+	while (1) {
 		xQueueReceive(xQueueUart, &cmdBuf, portMAX_DELAY);
 		ESP_LOGD(pcTaskGetName(NULL), "cmdBuf.length=%d", cmdBuf.length);
-		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), cmdBuf.payload, cmdBuf.length, ESP_LOG_DEBUG);
+		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), cmdBuf.payload, cmdBuf.length,
+				       ESP_LOG_DEBUG);
 		int txBytes = uart_write_bytes(CONFIG_UART_NUM, cmdBuf.payload, cmdBuf.length);
 		if (txBytes != cmdBuf.length) {
-			ESP_LOGE(pcTaskGetName(NULL), "uart_write_bytes Fail. txBytes=%d cmdBuf.length=%d", txBytes, cmdBuf.length);
+			ESP_LOGE(pcTaskGetName(NULL),
+				 "uart_write_bytes Fail. txBytes=%d cmdBuf.length=%d", txBytes,
+				 cmdBuf.length);
 		}
 	} // end while
 
@@ -101,17 +141,20 @@ static void uart_tx_task(void* pvParameters)
 	vTaskDelete(NULL);
 }
 
-static void uart_rx_task(void* pvParameters)
+static void uart_rx_task(void *pvParameters)
 {
-	ESP_LOGI(pcTaskGetName(NULL), "Start using UART%d(GPIO%d)", CONFIG_UART_NUM, CONFIG_UART_RX_GPIO);
+	ESP_LOGI(pcTaskGetName(NULL), "Start using UART%d(GPIO%d)", CONFIG_UART_NUM,
+		 CONFIG_UART_RX_GPIO);
 	CMD_t cmdBuf;
 	cmdBuf.spp_event_id = BLE_UART_EVT;
 	while (1) {
-		cmdBuf.length = uart_read_bytes(CONFIG_UART_NUM, cmdBuf.payload, PAYLOAD_SIZE, 10 / portTICK_PERIOD_MS);
+		cmdBuf.length = uart_read_bytes(CONFIG_UART_NUM, cmdBuf.payload, PAYLOAD_SIZE,
+						10 / portTICK_PERIOD_MS);
 		// There is some rxBuf in rx buffer
 		if (cmdBuf.length > 0) {
 			ESP_LOGD(pcTaskGetName(NULL), "cmdBuf.length=%d", cmdBuf.length);
-			ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), cmdBuf.payload, cmdBuf.length, ESP_LOG_DEBUG);
+			ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), cmdBuf.payload, cmdBuf.length,
+					       ESP_LOG_DEBUG);
 			BaseType_t err = xQueueSend(xQueueSpp, &cmdBuf, portMAX_DELAY);
 			if (err != pdTRUE) {
 				ESP_LOGE(pcTaskGetName(NULL), "xQueueSend Fail");
@@ -126,7 +169,7 @@ static void uart_rx_task(void* pvParameters)
 	vTaskDelete(NULL);
 }
 
-void nimble_spp_task(void * pvParameters);
+void nimble_spp_task(void *pvParameters);
 
 void initialize_nvs(void)
 {
@@ -147,24 +190,22 @@ void app_main(void)
 
 	// Create Queue
 	xQueueSpp = xQueueCreate(10, sizeof(CMD_t));
-	configASSERT( xQueueSpp );
-	xQueueUart = xQueueCreate( 10, sizeof(CMD_t) );
-	configASSERT( xQueueUart );
+	configASSERT(xQueueSpp);
+	xQueueUart = xQueueCreate(10, sizeof(CMD_t));
+	configASSERT(xQueueUart);
 
 	// Initialize UART
 	uart_init();
 
 	// Start tasks
-	xTaskCreate(uart_tx_task, "UART-TX", 1024*4, NULL, 2, NULL);
-	xTaskCreate(uart_rx_task, "UART-RX", 1024*4, NULL, 2, NULL);
-	xTaskCreate(nimble_spp_task, "NIMBLE_SPP", 1024*4, NULL, 2, NULL);
+	xTaskCreate(uart_tx_task, "UART-TX", 1024 * 4, NULL, 2, NULL);
+	xTaskCreate(uart_rx_task, "UART-RX", 1024 * 4, NULL, 2, NULL);
+	xTaskCreate(nimble_spp_task, "NIMBLE_SPP", 1024 * 4, NULL, 2, NULL);
 
-	ESP_ERROR_CHECK(console_cmd_init());     // Initialize console
+	ESP_ERROR_CHECK(console_cmd_init()); // Initialize console
 	register_system();
 	register_nvs();
+	register_commands();
 
-	// Register any other plugin command added to your project
-	ESP_ERROR_CHECK(console_cmd_all_register());
-
-	ESP_ERROR_CHECK(console_cmd_start());    // Start console
+	ESP_ERROR_CHECK(console_cmd_start()); // Start console
 }
