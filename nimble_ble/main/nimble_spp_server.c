7942b33cb8a330eac9ef8ae4fe84b03ef20cb8c6
Alex Kiernan <alex.kiernan@gmail.com>
2025-10-15 16:05:17 +0100

    Convert to use ble_conn_mgr

--- nimble_ble/main/nimble_spp_server.c
+++ nimble_ble/main/nimble_spp_server.c
@@ -1,4 +1,4 @@
-/*	ESP-NimBLE SPP Server Example
+/*	ESP-BLE SPP Server using ble_conn_mgr
 
 	This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -14,25 +14,16 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "esp_log.h"
+#include "esp_event.h"
 
-/* BLE */
-#include "nimble/nimble_port.h"
-#include "nimble/nimble_port_freertos.h"
-#include "host/ble_hs.h"
-#include "host/util/util.h"
-#include "console/console.h"
-#include "services/gap/ble_svc_gap.h"
-#include "services/gatt/ble_svc_gatt.h"
+#include "esp_ble_conn_mgr.h"
 
 #include "spp.h"
 
-static int ble_spp_server_gap_event(struct ble_gap_event *event, void *arg);
-static uint8_t own_addr_type;
-int gatt_svr_register(void);
-static bool conn_handle_subs[CONFIG_BT_NIMBLE_MAX_CONNECTIONS + 1];
-static uint16_t ble_spp_svc_gatt_read_val_handle;
+static esp_err_t esp_spp_chr_cb(const uint8_t *inbuf, uint16_t inlen, uint8_t **outbuf,
+				uint16_t *outlen, void *priv_data);
 
-#define DEVICE_NAME "ESP_NIMBLE_SERVER" //The Device Name Characteristics in GAP
+#define DEVICE_NAME "ESP_BLE_SERVER" //The Device Name Characteristics in GAP
 
 // 16 Bit SPP Service UUID
 #define BLE_SVC_SPP_UUID16 0xABF0
@@ -47,410 +38,143 @@ extern QueueHandle_t uart_tx_queue;
 
 #define RX_BUF_SIZE 128
 
-void ble_store_config_init(void);
+#ifdef CONFIG_BT_NIMBLE_ENABLED
+#define ATTRIBUTE_MAX_CONNECTIONS CONFIG_BT_NIMBLE_MAX_CONNECTIONS
+#else
+#define ATTRIBUTE_MAX_CONNECTIONS CONFIG_BT_ACL_CONNECTIONS
+#endif
 
-/**
- * Logs information about a connection to the console.
- */
-static void
-ble_spp_server_print_conn_desc(struct ble_gap_conn_desc *desc)
+uint16_t connection_handle[ATTRIBUTE_MAX_CONNECTIONS];
+
+static esp_err_t esp_spp_chr_cb(const uint8_t *inbuf, uint16_t inlen, uint8_t **outbuf,
+				uint16_t *outlen, void *priv_data)
 {
-	ESP_LOGI(pcTaskGetName(NULL), "handle=%d our_ota_addr_type=%d our_ota_addr=",
-		desc->conn_handle, desc->our_ota_addr.type);
-	//print_addr(desc->our_ota_addr.val);
-	ESP_LOGI(pcTaskGetName(NULL), "our_id_addr_type=%d our_id_addr=",
-		desc->our_id_addr.type);
-	//print_addr(desc->our_id_addr.val);
-	ESP_LOGI(pcTaskGetName(NULL), "peer_ota_addr_type=%d peer_ota_addr=",
-		desc->peer_ota_addr.type);
-	//print_addr(desc->peer_ota_addr.val);
-	ESP_LOGI(pcTaskGetName(NULL), "peer_id_addr_type=%d peer_id_addr=",
-		desc->peer_id_addr.type);
-	//print_addr(desc->peer_id_addr.val);
-	ESP_LOGI(pcTaskGetName(NULL), "conn_itvl=%d conn_latency=%d supervision_timeout=%d "
-		"encrypted=%d authenticated=%d bonded=%d",
-		desc->conn_itvl, desc->conn_latency,
-		desc->supervision_timeout,
-		desc->sec_state.encrypted,
-		desc->sec_state.authenticated,
-		desc->sec_state.bonded);
-}
-
-/**
- * Enables advertising with the following parameters:
- *	   o General discoverable mode.
- *	   o Undirected connectable mode.
- */
-static void
-ble_spp_server_advertise(void)
-{
-	struct ble_gap_adv_params adv_params;
-	struct ble_hs_adv_fields fields;
-	const char *name;
-	int rc;
-
-	/**
-	 *	Set the advertisement data included in our advertisements:
-	 *	   o Flags (indicates advertisement type and other general info).
-	 *	   o Advertising tx power.
-	 *	   o Device name.
-	 *	   o 16-bit service UUIDs (alert notifications).
-	 */
-
-	memset(&fields, 0, sizeof fields);
-
-	/* Advertise two flags:
-	 *	   o Discoverability in forthcoming advertisement (general)
-	 *	   o BLE-only (BR/EDR unsupported).
-	 */
-	fields.flags = BLE_HS_ADV_F_DISC_GEN |
-				   BLE_HS_ADV_F_BREDR_UNSUP;
-
-	/* Indicate that the TX power level field should be included; have the
-	 * stack fill this value automatically.  This is done by assigning the
-	 * special value BLE_HS_ADV_TX_PWR_LVL_AUTO.
-	 */
-	fields.tx_pwr_lvl_is_present = 1;
-	fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
-
-	name = ble_svc_gap_device_name();
-	fields.name = (uint8_t *)name;
-	fields.name_len = strlen(name);
-	fields.name_is_complete = 1;
-
-	fields.uuids16 = (ble_uuid16_t[]) {
-		BLE_UUID16_INIT(BLE_SVC_SPP_UUID16)
-	};
-	fields.num_uuids16 = 1;
-	fields.uuids16_is_complete = 1;
-
-	rc = ble_gap_adv_set_fields(&fields);
-	if (rc != 0) {
-		ESP_LOGE(pcTaskGetName(NULL), "error setting advertisement data; rc=%d", rc);
-		return;
+	if (!outbuf || !outlen) {
+		return ESP_ERR_INVALID_ARG;
 	}
 
-	/* Begin advertising. */
-	memset(&adv_params, 0, sizeof adv_params);
-	adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
-	adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
-	rc = ble_gap_adv_start(own_addr_type, NULL, BLE_HS_FOREVER,
-						   &adv_params, ble_spp_server_gap_event, NULL);
-	if (rc != 0) {
-		ESP_LOGE(pcTaskGetName(NULL), "error enabling advertisement; rc=%d", rc);
-		return;
-	}
-}
+	if (!inbuf) {
+		ESP_LOGI(TAG, "Callback for read");
+		*outlen = strlen("SPP_CHR");
+		*outbuf = (uint8_t *)strndup("SPP_CHR", *outlen);
+	} else {
+		ESP_LOGI(TAG, "Callback for write");
+		*outbuf = calloc(1, inlen);
+		memcpy(*outbuf, inbuf, inlen);
+		*outlen = inlen;
 
-/**
- * The nimble host executes this callback when a GAP event occurs.	The
- * application associates a GAP event callback with each connection that forms.
- * ble_spp_server uses the same callback for all connections.
- *
- * @param event					The type of event being signalled.
- * @param ctxt					Various information pertaining to the event.
- * @param arg					Application-specified argument; unused by
- *									ble_spp_server.
- *
- * @return						0 if the application successfully handled the
- *									event; nonzero on failure.	The semantics
- *									of the return code is specific to the
- *									particular GAP event being signalled.
- */
-static int
-ble_spp_server_gap_event(struct ble_gap_event *event, void *arg)
-{
-	struct ble_gap_conn_desc desc;
-	int rc;
-
-	switch (event->type) {
-	case BLE_GAP_EVENT_LINK_ESTAB:
-		/* A new connection was established or a connection attempt failed. */
-		ESP_LOGI(__FUNCTION__, "connection %s; status=%d ",
-			event->connect.status == 0 ? "established" : "failed",
-			event->connect.status);
-		if (event->connect.status == 0) {
-			rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
-			assert(rc == 0);
-			ble_spp_server_print_conn_desc(&desc);
-		}
-		if (event->connect.status != 0 || CONFIG_BT_NIMBLE_MAX_CONNECTIONS > 1) {
-			/* Connection failed or if multiple connection allowed; resume advertising. */
-			ble_spp_server_advertise();
-		}
-		return 0;
-
-	case BLE_GAP_EVENT_DISCONNECT:
-		ESP_LOGI(__FUNCTION__, "disconnect; reason=%d ", event->disconnect.reason);
-		ble_spp_server_print_conn_desc(&event->disconnect.conn);
-
-		conn_handle_subs[event->disconnect.conn.conn_handle] = false;
-
-		/* Connection terminated; resume advertising. */
-		ble_spp_server_advertise();
-		return 0;
-
-	case BLE_GAP_EVENT_CONN_UPDATE:
-		/* The central has updated the connection parameters. */
-		ESP_LOGI(__FUNCTION__, "connection updated; status=%d ", event->conn_update.status);
-		rc = ble_gap_conn_find(event->conn_update.conn_handle, &desc);
-		assert(rc == 0);
-		ble_spp_server_print_conn_desc(&desc);
-		return 0;
-
-	case BLE_GAP_EVENT_ADV_COMPLETE:
-		ESP_LOGI(__FUNCTION__, "advertise complete; reason=%d", event->adv_complete.reason);
-		ble_spp_server_advertise();
-		return 0;
-
-	case BLE_GAP_EVENT_MTU:
-		ESP_LOGI(__FUNCTION__, "mtu update event; conn_handle=%d cid=%d mtu=%d",
-			event->mtu.conn_handle,
-			event->mtu.channel_id,
-			event->mtu.value);
-		return 0;
-
-	case BLE_GAP_EVENT_SUBSCRIBE:
-		ESP_LOGI(__FUNCTION__, "subscribe event; conn_handle=%d attr_handle=%d "
-			"reason=%d prevn=%d curn=%d previ=%d curi=%d",
-			event->subscribe.conn_handle,
-			event->subscribe.attr_handle,
-			event->subscribe.reason,
-			event->subscribe.prev_notify,
-			event->subscribe.cur_notify,
-			event->subscribe.prev_indicate,
-			event->subscribe.cur_indicate);
-		conn_handle_subs[event->subscribe.conn_handle] = true;
-		return 0;
-
-	default:
-		return 0;
-	}
-}
-
-static void
-ble_spp_server_on_reset(int reason)
-{
-	ESP_LOGE(__FUNCTION__, "Resetting state; reason=%d", reason);
-}
-
-static void
-ble_spp_server_on_sync(void)
-{
-	int rc;
-
-	rc = ble_hs_util_ensure_addr(0);
-	assert(rc == 0);
-
-	/* Figure out address to use while advertising (no privacy for now) */
-	rc = ble_hs_id_infer_auto(0, &own_addr_type);
-	if (rc != 0) {
-		ESP_LOGE(__FUNCTION__, "error determining address type; rc=%d", rc);
-		return;
-	}
-
-	/* Printing ADDR */
-	uint8_t addr_val[6] = {0};
-	rc = ble_hs_id_copy_addr(own_addr_type, addr_val, NULL);
-
-	ESP_LOGI(__FUNCTION__, "Device Address: %02x:%02x:%02x:%02x:%02x:%02x",
-		addr_val[0], addr_val[1], addr_val[2], addr_val[3], addr_val[4], addr_val[5]);
-	/* Begin advertising. */
-	ble_spp_server_advertise();
-}
-
-void ble_spp_server_host_task(void *param)
-{
-	ESP_LOGI(TAG, "BLE Host Task Started");
-	/* This function will return only when nimble_port_stop() is executed */
-	nimble_port_run();
-
-	nimble_port_freertos_deinit();
-}
-
-/* Callback function for custom service */
-static int ble_svc_gatt_handler(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
-{
-	switch (ctxt->op) {
-	case BLE_GATT_ACCESS_OP_READ_CHR:
-		ESP_LOGI(__FUNCTION__, "Callback for read");
-		break;
-
-	case BLE_GATT_ACCESS_OP_WRITE_CHR:
-		ESP_LOGD(__FUNCTION__, "Data received in write event,conn_handle = %x,attr_handle = %x", conn_handle, attr_handle);
-		ESP_LOGD(__FUNCTION__, "Data received in write event,ctxt->om->om_len = %d", ctxt->om->om_len);
-		ESP_LOGD(__FUNCTION__, "Data received in write event,ctxt->om->om_data = [%.*s]",ctxt->om->om_len, ctxt->om->om_data);
-		ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, ctxt->om->om_data, ctxt->om->om_len, ESP_LOG_DEBUG);
-
-		/*
-		[61 62 63 64 65 66 67 0d 0a] to [61 62 63 64 65 66 67 0a]
-		*/
+		// Send data to UART TX queue
 		CMD_t cmdBuf;
-		cmdBuf.length = 0;
-		for (int i=0;i<ctxt->om->om_len;i++) {
-			if (cmdBuf.length < PAYLOAD_SIZE) {
-				cmdBuf.payload[cmdBuf.length] = ctxt->om->om_data[i];
-				cmdBuf.length++;
-			}
-		}
-		ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, cmdBuf.payload, cmdBuf.length, ESP_LOG_DEBUG);
-		BaseType_t err = xQueueSendFromISR(uart_tx_queue, &cmdBuf, NULL);
+		cmdBuf.length = inlen;
+		memcpy(cmdBuf.payload, inbuf, inlen);
+		BaseType_t err = xQueueSend(uart_tx_queue, &cmdBuf, portMAX_DELAY);
 		if (err != pdTRUE) {
-			ESP_LOGE(__FUNCTION__, "xQueueSendFromISR Fail");
+			ESP_LOGE(TAG, "xQueueSend Fail");
 		}
-		break;
-
-	default:
-		ESP_LOGI(__FUNCTION__, "Default Callback");
-		break;
 	}
-	return 0;
+
+	return ESP_OK;
 }
 
-/* Define new custom service */
-static const struct ble_gatt_svc_def new_ble_svc_gatt_defs[] = {
-	{
-		/*** Service: SPP */
-		.type = BLE_GATT_SVC_TYPE_PRIMARY,
-		.uuid = BLE_UUID16_DECLARE(BLE_SVC_SPP_UUID16),
-		.characteristics = (struct ble_gatt_chr_def[])
-		{ {
-				/* Support SPP service */
-				.uuid = BLE_UUID16_DECLARE(BLE_SVC_SPP_CHR_UUID16),
-				.access_cb = ble_svc_gatt_handler,
-				.val_handle = &ble_spp_svc_gatt_read_val_handle,
-				.flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_WRITE | BLE_GATT_CHR_F_NOTIFY,
-			}, {
-				0, /* No more characteristics */
-			}
-		},
-	},
-	{
-		0, /* No more services. */
-	},
+static void app_ble_conn_event_handler(void *handler_args, esp_event_base_t base, int32_t id,
+				       void *event_data)
+{
+	if (base != BLE_CONN_MGR_EVENTS) {
+		return;
+	}
+
+	switch (id) {
+	case ESP_BLE_CONN_EVENT_CONNECTED:
+		ESP_LOGI(TAG, "ESP_BLE_CONN_EVENT_CONNECTED\n");
+		connection_handle[0] = 1;
+		break;
+	case ESP_BLE_CONN_EVENT_DISCONNECTED:
+		ESP_LOGI(TAG, "ESP_BLE_CONN_EVENT_DISCONNECTED\n");
+		connection_handle[0] = 0;
+		break;
+	default:
+		break;
+	}
+}
+
+static const esp_ble_conn_character_t spp_nu_lookup_table[] = {
+	{ "spp_chr",
+	  BLE_CONN_UUID_TYPE_16,
+	  BLE_CONN_GATT_CHR_READ | BLE_CONN_GATT_CHR_WRITE | BLE_CONN_GATT_CHR_NOTIFY |
+		  BLE_CONN_GATT_CHR_INDICATE,
+	  { BLE_SVC_SPP_CHR_UUID16 },
+	  esp_spp_chr_cb },
 };
 
-static void
-gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg)
+static const esp_ble_conn_svc_t spp_svc = {
+    .type = BLE_CONN_UUID_TYPE_16,
+    .uuid = {
+        .uuid16 = BLE_SVC_SPP_UUID16,
+    },
+    .nu_lookup_count = sizeof(spp_nu_lookup_table) / sizeof(spp_nu_lookup_table[0]),
+    .nu_lookup = (esp_ble_conn_character_t *)spp_nu_lookup_table
+};
+
+int gatt_svr_register(void)
 {
-	char buf[BLE_UUID_STR_LEN];
-
-	switch (ctxt->op) {
-	case BLE_GATT_REGISTER_OP_SVC:
-		ESP_LOGD(__FUNCTION__, "registered service %s with handle=%d",
-			ble_uuid_to_str(ctxt->svc.svc_def->uuid, buf),
-			ctxt->svc.handle);
-		break;
-
-	case BLE_GATT_REGISTER_OP_CHR:
-		ESP_LOGD(__FUNCTION__, "registering characteristic %s with "
-			"def_handle=%d val_handle=%d\n",
-			ble_uuid_to_str(ctxt->chr.chr_def->uuid, buf),
-			ctxt->chr.def_handle,
-			ctxt->chr.val_handle);
-		break;
-
-	case BLE_GATT_REGISTER_OP_DSC:
-		ESP_LOGD(__FUNCTION__, "registering descriptor %s with handle=%d",
-			ble_uuid_to_str(ctxt->dsc.dsc_def->uuid, buf),
-			ctxt->dsc.handle);
-		break;
-
-	default:
-		assert(0);
-		break;
-	}
+	return esp_ble_conn_add_svc(&spp_svc);
 }
 
-int gatt_svr_init(void)
+void nimble_spp_task(void *pvParameters)
 {
-	int rc = 0;
-	ble_svc_gap_init();
-	ble_svc_gatt_init();
+	ESP_LOGI(TAG, "BLE SPP Task Started");
 
-	rc = ble_gatts_count_cfg(new_ble_svc_gatt_defs);
+	esp_ble_conn_config_t config = {
+		.device_name = DEVICE_NAME,
+		.include_service_uuid = 1,
+		.adv_uuid16 = BLE_SVC_SPP_UUID16,
+	};
 
-	if (rc != 0) {
-		return rc;
+	esp_err_t ret;
+
+	/* Initialize connection_handle array */
+	for (int i = 0; i < ATTRIBUTE_MAX_CONNECTIONS; i++) {
+		connection_handle[i] = 0;
 	}
 
-	rc = ble_gatts_add_svcs(new_ble_svc_gatt_defs);
-	if (rc != 0) {
-		return rc;
-	}
+	esp_event_handler_register(BLE_CONN_MGR_EVENTS, ESP_EVENT_ANY_ID,
+				   app_ble_conn_event_handler, NULL);
 
-	return 0;
-}
+	esp_ble_conn_init(&config);
 
-void nimble_spp_task(void * pvParameters)
-{
-	ESP_LOGI(pcTaskGetName(0), "Start");
-	esp_err_t ret = nimble_port_init();
-	if (ret != ESP_OK) {
-		ESP_LOGE(pcTaskGetName(NULL), "Failed to init nimble %d", ret);
+	ret = gatt_svr_register();
+	assert(ret == 0);
+
+	if (esp_ble_conn_start() != ESP_OK) {
+		esp_ble_conn_stop();
+		esp_ble_conn_deinit();
+		esp_event_handler_unregister(BLE_CONN_MGR_EVENTS, ESP_EVENT_ANY_ID,
+					     app_ble_conn_event_handler);
 		return;
 	}
 
-	/* Initialize connection_handle array */
-	for (int i = 0; i <= CONFIG_BT_NIMBLE_MAX_CONNECTIONS; i++) {
-		conn_handle_subs[i] = false;
-	}
-
-	/* Initialize the NimBLE host configuration. */
-	ble_hs_cfg.reset_cb = ble_spp_server_on_reset;
-	ble_hs_cfg.sync_cb = ble_spp_server_on_sync;
-	ble_hs_cfg.gatts_register_cb = gatt_svr_register_cb;
-	ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
-
-	//ble_hs_cfg.sm_io_cap = CONFIG_EXAMPLE_IO_TYPE;
-#ifdef CONFIG_EXAMPLE_BONDING
-	ble_hs_cfg.sm_bonding = 1;
-#endif
-#ifdef CONFIG_EXAMPLE_MITM
-	ble_hs_cfg.sm_mitm = 1;
-#endif
-#ifdef CONFIG_EXAMPLE_USE_SC
-	ble_hs_cfg.sm_sc = 1;
-#else
-	ble_hs_cfg.sm_sc = 0;
-#endif
-	ESP_LOGI(pcTaskGetName(NULL), "sm_io_cap=%d sm_bonding=%d sm_mitm=%d sm_sc=%d",
-		ble_hs_cfg.sm_io_cap, ble_hs_cfg.sm_bonding, ble_hs_cfg.sm_mitm, ble_hs_cfg.sm_sc);
-#ifdef CONFIG_EXAMPLE_BONDING
-	ble_hs_cfg.sm_our_key_dist = 1;
-	ble_hs_cfg.sm_their_key_dist = 1;
-#endif
-
-	/* Register custom service */
-	int rc = gatt_svr_init();
-	assert(rc == 0);
-
-	/* Set the default device name. */
-	//rc = ble_svc_gap_device_name_set("NIMBLE_SPP_SERVER");
-	rc = ble_svc_gap_device_name_set(DEVICE_NAME);
-	assert(rc == 0);
-
-	/* XXX Need to have template for store */
-	ble_store_config_init();
-
-	nimble_port_freertos_init(ble_spp_server_host_task);
-
 	CMD_t cmdBuf;
-	while(1){
+	while (1) {
 		xQueueReceive(xQueueSpp, &cmdBuf, portMAX_DELAY);
-		ESP_LOGD(pcTaskGetName(NULL), "cmdBuf.spp_event_id=%d", cmdBuf.spp_event_id);
+		ESP_LOGD(TAG, "cmdBuf.spp_event_id=%d", cmdBuf.spp_event_id);
 		if (cmdBuf.spp_event_id == BLE_UART_EVT) {
-
-			for (int i = 0; i <= CONFIG_BT_NIMBLE_MAX_CONNECTIONS; i++) {
-				/* Check if client has subscribed to notifications */
-				if (conn_handle_subs[i]) {
-					struct os_mbuf *txom;
-					txom = ble_hs_mbuf_from_flat(cmdBuf.payload, cmdBuf.length);
-					int rc = ble_gatts_notify_custom(i, ble_spp_svc_gatt_read_val_handle, txom);
+			for (int i = 0; i < ATTRIBUTE_MAX_CONNECTIONS; i++) {
+				if (connection_handle[i] != 0) {
+					esp_ble_conn_data_t inbuff = {
+						.type = BLE_CONN_UUID_TYPE_16,
+						.uuid = {
+							.uuid16 = BLE_SVC_SPP_CHR_UUID16,
+						},
+						.data = cmdBuf.payload,
+						.data_len = cmdBuf.length,
+					};
+					int rc = esp_ble_conn_notify(&inbuff);
 					if (rc == 0) {
-						ESP_LOGD(pcTaskGetName(NULL), "Notification sent successfully");
+						ESP_LOGD(TAG, "Notification sent successfully");
 					} else {
-						ESP_LOGI(pcTaskGetName(NULL), "Error in sending notification rc = %d", rc);
+						ESP_LOGI(TAG,
+							 "Error in sending notification rc = %d",
+							 rc);
 					}
 				}
 			}
